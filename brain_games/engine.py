913823c3d71ff5c6ef76b115be4ac30e711784ac
Kloym <oleg.petr1223@gmail.com>
2024-10-17 20:55:47 +0300

    reworked

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,11 +4,11 @@ from brain_games.cli import welcome_user
 
 def run_game(game):
     name = welcome_user()
+    print(game.game_quest)
     count = 0
     while count < 3:
         question, answer = game.question_and_answer()
-        print(game.game_quest)
-        print(f"Question: {question}")
+        print(f'Question: {question}')
         user_input = prompt.string("Your answer: ")
         if user_input.lower() != str(answer):
             print(f"'{user_input}' is wrong answer ;(. Correct answer was '{answer}'.")
